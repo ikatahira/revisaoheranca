16f3cf58589b9e7b3fc30c746573291d3977d88a
ikatahira <isaque.katahira@fatec.sp.gov.br>
2022-02-22 17:34:06 +0000

    adicionando arquivo

---
+++ Pessoa.java
@@ -0,0 +1,27 @@
+public class Pessoa{
+
+    private String nome;
+    private String endereco;
+    private String telefone;
+
+    public String getNome(){
+        return this.nome;
+    }
+    public void setNome(String nome){
+        this.nome=nome;
+    }
+    
+    public String getEndereco(){
+        return this.endereco;
+    }
+    public void setEndereco(String endereco){
+        this.endereco=endereco;
+    }
+    
+    public String getTelefone(){
+        return this.telefone;
+    }
+    public void setTelefone(String telefone){
+        this.telefone=telefone;
+    }
+}
