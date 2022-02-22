9e04b1d27b7e112eb47473d36111ee7f35bee0cd
ikatahira <isaque.katahira@fatec.sp.gov.br>
2022-02-22 17:50:20 +0000

    inclusão da classe Empregado

---
+++ Empregado.java
@@ -0,0 +1,13 @@
+public class Empregado extends Pessoa{
+
+    private double salario;
+
+    public double getSalario(){
+        return this.salario;
+    }
+
+    public void setSalario(double salario){
+        this.salario=salario;
+    }
+    
+}
